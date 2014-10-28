6fd69c553f3041439df4aca6ac0247f3b4ba4f1e
danielAparicio <daniel@daniels-mbp.home>
2014-10-28 21:17:54 +0000

    adding an script to do the sbt eclipse for all projects

---
+++ sbtEclipseAll.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd streams
+sbt eclipse
+cd ..
+cd recfun
+sbt eclipse
+cd ..
+cd progfun
+sbt eclipse
+cd ..
+cd patmat
+sbt eclipse
+cd ..
+cd objsets
+sbt eclipse
+cd ..
+cd funsets
+sbt eclipse
+cd ..
+cd forcomp
+sbt eclipse
+cd ..
+cd example
+sbt eclipse
